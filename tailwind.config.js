9dad7c6954daa2d7ca61613e04c7977322b507d6
11saran <1211saran@gmail.com>
2024-12-11 21:22:16 +0530

    #2,Top Bar added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        acumin: ["acumin-pro", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
